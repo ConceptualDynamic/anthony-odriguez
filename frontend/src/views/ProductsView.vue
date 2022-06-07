b1768122ce1846f52ee5b552f563fbfab8251ed2
anthonygalin <80184532+anthonygalin@users.noreply.github.com>
2022-06-07 00:58:17 -0400

    created client and product page and route.

---
+++ frontend/src/views/ProductsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="products-view">
+    <div>
+      Esto son los productos
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
